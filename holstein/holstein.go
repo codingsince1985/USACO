ed36b9a6ea2014219723059ec5b33394bde23801
Jerry Zhao <jchao@y7mail.com>
2015-10-10 17:32:44 +1100

    code clean up

--- holstein/holstein.go
+++ holstein/holstein.go
@@ -17,7 +17,15 @@ var g [][]int
 var minCount int
 var answer []int
 
-func valid(total []int) bool {
+func newTotal(total []int, current int) []int {
+	newTotal := make([]int, V)
+	for i := range total {
+		newTotal[i] = total[i] + g[current][i]
+	}
+	return newTotal
+}
+
+func isEnough(total []int) bool {
 	for i := range total {
 		if total[i] < v[i] {
 			return false
@@ -26,45 +34,38 @@ func valid(total []int) bool {
 	return true
 }
 
-func dfs(used, total []int, depth int) {
-	if (depth == G) {
+func dfs(used, total []int, current int) {
+	if (current == G) {
 		return
 	}
 
-	keepUsed := make([]int, len(used));
-	copy(keepUsed, used);
-	keepTotal := make([]int, V, V)
-	copy(keepTotal, total)
-
-	used = append(used, depth)
-	for i := range total {
-		total[i] += g[depth][i]
+	newUsed := append(used, current)
+	if (len(newUsed) >= minCount) {
+		return
 	}
+	newTotal := newTotal(total, current)
 
-	if valid(total) {
-		if (minCount > len(used)) {
-			//			fmt.Println(used)
-			//			fmt.Println(total)
-			minCount = len(used)
-			answer = used
+	if isEnough(newTotal) {
+		if (len(newUsed) < minCount) {
+			//			fmt.Println(newUsed)
+			//			fmt.Println(newTotal)
+			minCount = len(newUsed)
+			answer = make([]int, len(newUsed))
+			copy(answer, newUsed)
 		}
 		return
 	}
 
-	dfs(used, total, depth + 1)
+	dfs(newUsed, newTotal, current + 1)
 
-	used = make([]int, len(keepUsed));
-	copy(used, keepUsed);
-	copy(total, keepTotal)
-
-	dfs(used, total, depth + 1)
+	dfs(used, total, current + 1)
 }
 
 func input() {
 	in, _ := os.Open("holstein.in")
 	fmt.Fscanf(in, "%d", &V)
 
-	v = make([]int, V, V)
+	v = make([]int, V)
 	for i := 0; i < V; i ++ {
 		fmt.Fscanf(in, "%d", &v[i])
 	}
@@ -72,9 +73,9 @@ func input() {
 	fmt.Fscanf(in, "%d", &G)
 	minCount = G + 1
 
-	g = make([][]int, G, G)
+	g = make([][]int, G)
 	for i := 0; i < G; i ++ {
-		g[i] = make([]int, V, V)
+		g[i] = make([]int, V)
 		for j := 0; j < V; j ++ {
 			fmt.Fscanf(in, "%d", &g[i][j])
 		}
@@ -94,8 +95,6 @@ func output() {
 
 func main() {
 	input()
-	used := make([]int, 0)
-	total := make([]int, V, V)
-	dfs(used, total, 0)
+	dfs(make([]int, 0), make([]int, V), 0)
 	output()
 }
